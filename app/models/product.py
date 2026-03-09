2f11b388bd029a1c511c6d6619d95404c82eb01a
Njyhier <njerinjihia43@gmail.com>
2026-03-09 10:23:38 +0300

    changed id type from UUID to string

--- app/models/product.py
+++ app/models/product.py
@@ -1,12 +1,12 @@
 from sqlalchemy import Column, String, UUID, Integer, Text, Numeric
-from Base import BaseModel
+from .Base import BaseModel
 import uuid
 
 
 class Product(BaseModel):
     __tablename__ = "products"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    id = Column(String(60), primary_key=True, default=lambda: str(uuid.uuid4()))
     name = Column(String)
     price = Column(Numeric(10, 2))
     description = Column(Text)
