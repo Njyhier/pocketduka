a7dd113111d720e999e06dd1f61f3a0bdd177bf4
Njyhier <njerinjihia43@gmail.com>
2026-03-16 01:49:37 +0300

    modified update_user_roles to match get roles by names

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -2,20 +2,30 @@ from app.schemas.user_schemas import (
     UserWrite,
     UserUpdate,
 )
+from app.schemas.role_schemas import UserRoleUpdate
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import HTTPException, status
 from app.models.user import User
-from sqlalchemy import select
+from sqlalchemy import select, or_
 from app.utils.password import get_password_hash
+from app.utils.user_utils import get_user_by_user_id
+from .role_service import read_roles_by_names, get_role_by_name
 
 
 async def create_user(session: AsyncSession, user_create: UserWrite) -> User:
-    res = await session.execute(select(User).where(User.email == user_create.email))
+    res = await session.execute(
+        select(User).where(
+            or_(
+                User.email == user_create.email,
+                User.username == user_create.username,
+            )
+        )
+    )
     user = res.scalar_one_or_none()
     if user:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
-            detail="Email already exists",
+            detail="User already exists",
         )
     db_user = User(
         first_name=user_create.first_name,
@@ -33,39 +43,15 @@ async def create_user(session: AsyncSession, user_create: UserWrite) -> User:
 async def read_users(session: AsyncSession) -> list[User]:
     result = await session.execute(select(User))
     users = result.scalars().all()
-    if not users:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="No users were found"
-        )
     return users
 
 
-async def read_user(session: AsyncSession, user_id: str) -> User:
-    result = await session.execute(select(User).where(User.id == user_id))
-    user = result.scalar_one_or_none()
-    if user is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="User not found",
-        )
-    return user
-
-
-async def read_user_me(user: User) -> User:
-    return user
-
-
 async def update_user(
-    user_update: UserUpdate, user_id: str, session: AsyncSession
+    user_update: UserUpdate,
+    user_id: str,
+    session: AsyncSession,
 ) -> User:
-
-    result = await session.execute(select(User).where(User.id == user_id))
-    user_to_update = result.scalar_one_or_none()
-    if user_to_update is None:
-        raise HTTPException(
-            status_code=404,
-            detail="User not found",
-        )
+    user_to_update = await get_user_by_user_id(user_id, session)
     update_data = user_update.model_dump(exclude_unset=True)
     for key, value in update_data.items():
         setattr(user_to_update, key, value)
@@ -92,5 +78,21 @@ async def delete_user(
         )
     await session.delete(user_to_delete)
     await session.commit()
-    await session.refresh()
     return {"message": "User deleted successfuly"}
+
+
+async def update_user_roles(
+    user_id: str, update_data: UserRoleUpdate, session: AsyncSession
+):
+    user_to_upgrade = await get_user_by_user_id(user_id, session=session)
+    roles = user_to_upgrade.roles
+    existing_roles = {role.id for role in roles}
+
+    for name in update_data.role_names:
+        role = await get_role_by_name(name, session)
+        if role not in roles:
+            roles.append(role)
+            existing_roles.add(role.id)
+    await session.commit()
+    await session.refresh(user_to_upgrade)
+    return user_to_upgrade
