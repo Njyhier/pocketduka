88db1b295d582f4d7151b8c5f7e2f72b4e463ce2
Njyhier <njerinjihia43@gmail.com>
2026-03-15 22:14:15 +0300

    added user_roles table

--- app/models/associations.py
+++ app/models/associations.py
@@ -4,6 +4,15 @@ from sqlalchemy import Column, ForeignKey, String, Integer
 
 class RolePermissions(BaseModel):
     __tablename__ = "role_permissions"
-    id= Column(Integer)
-    role_id = Column('role_id',String(60), ForeignKey("roles.id"), primary_key=True)
-    permission_id = Column('permission_id',String(60), ForeignKey("permissions.id"), primary_key=True)
+    id = Column(Integer)
+    role_id = Column("role_id", String(60), ForeignKey("roles.id"), primary_key=True)
+    permission_id = Column(
+        "permission_id", String(60), ForeignKey("permissions.id"), primary_key=True
+    )
+
+
+class UserRoles(BaseModel):
+    __tablename__ = "user_roles"
+    id = Column(Integer)
+    role_id = Column("role_id", String(60), ForeignKey("roles.id"), primary_key=True)
+    user_id = Column("user_id", String(60), ForeignKey("users.id"), primary_key=True)
