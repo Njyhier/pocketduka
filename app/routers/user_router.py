b48e50c94de24628dcaf48e803a72780edaa998a
Njyhier <njerinjihia43@gmail.com>
2026-03-15 23:16:07 +0300

    added  update_user_roles_route, edit_profile

--- app/routers/user_router.py
+++ app/routers/user_router.py
@@ -1,46 +1,55 @@
-from app.schemas.user_schemas import (
-    UserReadPrivate,
-    UserWrite,
-    UserReadPublic,
-    UserUpdate,
-)
+from app.schemas.role_schemas import UserRoleUpdate
 from app.schemas.Baseschema import DeleteResponce
-from app.services.user_service import (
-    read_users,
-    create_user,
-    read_user,
-    update_user,
-    delete_user,
+from app.utils.user_utils import (
+    get_user_by_username,
+    get_user_by_email,
+    get_user_by_user_id,
 )
+from fastapi import Depends, APIRouter
 from app.db.session import get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
-from fastapi import APIRouter, Depends
 from app.schemas.token_schemas import Token
 from typing import Annotated
 from fastapi.security import OAuth2PasswordRequestForm
-
+from app.services.auth_service import require_roles_dep, get_current_user
 from app.services.auth_service import login_for_access_token
+
+from app.schemas.user_schemas import (
+    UserReadPrivate,
+    UserWrite,
+    UserUpdate,
+    UserSignUp,
+)
+from app.services.user_service import (
+    read_users,
+    create_user,
+    update_user,
+    delete_user,
+    update_user_roles,
+)
+
 
 router = APIRouter()
 
 
-@router.post("/sign_in")
-async def sign_in(
-    user_data: UserWrite, session: Annotated[AsyncSession, Depends(get_async_session)]
+@router.post("/sign_up")
+async def sign_up(
+    user_data: UserSignUp,
+    session: AsyncSession = Depends(get_async_session),
 ):
     return await create_user(session, user_data)
 
 
-@router.post("/login", response_model=Token)
+@router.post("/token", response_model=Token)
 async def login(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
-    session: Annotated[AsyncSession, Depends(get_async_session)],
+    session: AsyncSession = Depends(get_async_session),
 ):
 
     return await login_for_access_token(form_data, session)
 
 
-@router.post("/users", response_model=UserReadPublic)
+@router.post("/users", response_model=UserReadPrivate)
 async def create_user_route(
     user_data: UserWrite,
     session: AsyncSession = Depends(get_async_session),
@@ -51,19 +60,37 @@ async def create_user_route(
 @router.get("/users", response_model=list[UserReadPrivate])
 async def read_users_route(
     session: AsyncSession = Depends(get_async_session),
-) -> list[UserReadPublic]:
+    _:bool = Depends(require_roles_dep('admin'))
+) -> list[UserReadPrivate]:
     return await read_users(session)
 
 
-@router.get("/users/{user_id}", response_model=UserReadPublic)
+@router.get("/users/by_email", response_model=UserReadPrivate)
+async def read_user_by_email(
+    email: str, session: AsyncSession = Depends(get_async_session)
+):
+    return await get_user_by_email(email, session)
+
+
+@router.get("/users/by_username", response_model=UserReadPrivate)
+async def get_user_by_username_route(
+    username: str, session: AsyncSession = Depends(get_async_session)
+):
+    user = await get_user_by_username(username, session)
+    return user
+
+
+@router.get("/users/{user_id}", response_model=UserReadPrivate)
 async def read_user_route(
     user_id: str,
     session: AsyncSession = Depends(get_async_session),
 ):
-    return await read_user(session, user_id)
+
+    user = await get_user_by_user_id(user_id, session)
+    return user
 
 
-@router.put("/users/{user_id}", response_model=UserReadPublic)
+@router.put("/users/{user_id}", response_model=UserReadPrivate)
 async def update_user_route(
     user_data: UserUpdate,
     user_id: str,
@@ -74,6 +101,27 @@ async def update_user_route(
 
 @router.delete("/users/{user_id}", response_model=DeleteResponce)
 async def delete_user_route(
-    user_id: str, session: AsyncSession = Depends(get_async_session)
+    user_id: str,
+    session: AsyncSession = Depends(get_async_session),
 ):
     return await delete_user(user_id, session)
+
+
+@router.patch("/users/{user_id}/roles")
+async def update_user_roles_route(
+    user_id: str,
+    role_update: UserRoleUpdate,
+    session: AsyncSession = Depends(get_async_session),
+):
+
+    return await update_user_roles(user_id, role_update.role_ids, session)
+
+
+@router.patch("/users/me", response_model=UserReadPrivate)
+async def edit_profile(
+    user_data: UserUpdate,
+    current_user: UserReadPrivate = Depends(get_current_user),
+    session: AsyncSession = Depends(get_async_session),
+):
+    user_id = current_user.id
+    return await update_user(user_data, user_id, session)
