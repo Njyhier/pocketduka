2014c9197ceb5ea2c9fb478cde2bc4585f6b8c71
Njyhier <njerinjihia43@gmail.com>
2026-03-15 22:50:18 +0300

    added created at and roles relationship

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, String, DateTime
+from sqlalchemy.orm import relationship
 from .Base import BaseModel
 from datetime import datetime, timezone
 import uuid
@@ -15,6 +16,8 @@ class User(BaseModel):
     first_name = Column(String)
     last_name = Column(String)
     email = Column(String, nullable=False, unique=True)
-    username = Column(String(12), nullable=False, unique=True)
+    username = Column(String(60), nullable=False, unique=True)
+    created_at = Column(DateTime, default=datetime.now(timezone.utc), nullable=False)
     updated_at = Column(DateTime, onupdate=lambda: datetime.now(timezone.utc))
     password_hash = Column(String(60))
+    roles = relationship("Role", secondary="user_roles", back_populates="users")
