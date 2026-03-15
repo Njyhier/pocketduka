a4715b4e5e4178979f685be0ad7505d3faca2c01
Njyhier <njerinjihia43@gmail.com>
2026-03-15 22:16:30 +0300

    added created at, updated at, user relationship, and made name unique

--- app/models/roles.py
+++ app/models/roles.py
@@ -1,8 +1,9 @@
-from .associations import RolePermissions
 from .Base import BaseModel
 from sqlalchemy import String, Column
 from sqlalchemy.orm import relationship
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import DateTime
+from datetime import datetime, timezone
 import uuid
 
 
@@ -10,7 +11,10 @@ class Role(BaseModel):
     __tablename__ = "roles"
 
     id = Column(String(60), primary_key=True, default=lambda: str(uuid.uuid4()))
-    name = Column(String(250), nullable=False)
+    name = Column(String(250), nullable=False, unique=True)
+    created_at = Column(DateTime, default=datetime.now(timezone.utc), nullable=False)
+    updated_at = Column(DateTime, onupdate=lambda: datetime.now(timezone.utc))
     permissions = relationship(
         "Permission", secondary="role_permissions", back_populates="roles"
     )
+    users = relationship("User", secondary="user_roles", back_populates="roles")
