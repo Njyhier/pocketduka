e765d1b4b112f4ee6baff4a8cffc59de5238bd6c
Njyhier <njerinjihia43@gmail.com>
2026-03-16 19:56:25 +0300

    added cors middleware

--- app/app.py
+++ app/app.py
@@ -7,6 +7,7 @@ from .routers import (
     permission_router,
     image_router,
 )
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 
 
@@ -17,6 +18,14 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods="*",
+    allow_headers=["*"],
+)
 
 app.include_router(user_router.router)
 app.include_router(product_router.router)
